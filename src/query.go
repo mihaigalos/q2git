bec08571b9b44147361ffea6942e189e730fc50e
Mihai Galos <mihai@galos.one>
2026-01-30 22:40:53 +0100

    chore: Remove superfluous

--- src/query.go
+++ src/query.go
@@ -7,21 +7,17 @@ import (
 	"github.com/itchyny/gojq"
 )
 
-// ExecuteQuery executes a jq query on the input data
 func ExecuteQuery(query string, data []byte) ([]byte, error) {
-	// Parse the jq query
 	jqQuery, err := gojq.Parse(query)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse jq query: %w", err)
 	}
 
-	// Unmarshal input data
 	var input interface{}
 	if err := json.Unmarshal(data, &input); err != nil {
 		return nil, fmt.Errorf("failed to parse input JSON: %w", err)
 	}
 
-	// Execute the query
 	var results []interface{}
 	iter := jqQuery.Run(input)
 	for {
@@ -35,7 +31,6 @@ func ExecuteQuery(query string, data []byte) ([]byte, error) {
 		results = append(results, v)
 	}
 
-	// If there's only one result, return it directly (not wrapped in array)
 	var output interface{}
 	if len(results) == 1 {
 		output = results[0]
@@ -43,6 +38,5 @@ func ExecuteQuery(query string, data []byte) ([]byte, error) {
 		output = results
 	}
 
-	// Marshal results back to JSON
 	return json.MarshalIndent(output, "", "  ")
 }
